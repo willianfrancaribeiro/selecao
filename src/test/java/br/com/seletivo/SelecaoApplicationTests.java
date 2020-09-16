f691def5e4b73a3047be8e3bab7203d2aab574e8
Willian Franca Ribeiro <willianribeiro@fiema.org.br>
2020-09-16 17:06:44 -0300

    testes

--- src/test/java/br/com/seletivo/SelecaoApplicationTests.java
+++ src/test/java/br/com/seletivo/SelecaoApplicationTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import br.com.seletivo.listener.DiretorioWatchInter;
+import br.com.seletivo.listener.ler.ArquivosManager;
 import br.com.seletivo.processa.ArquivoProcessa;
 
 
@@ -14,14 +15,24 @@ class SelecaoApplicationTests {
 
 	@Test
 	public void testeProcessamento() {
-		ArquivoProcessa.processaArquivos(DiretorioWatchInter.DIRETORIO_IN);
+		ArquivosManager.flag = true;
+		ArquivoProcessa.processaArquivos(DiretorioWatchInter.DIRETORIO_IN+"\\teste.txt");
 		assertEquals(ArquivoProcessa.getCliente(),2);
 		assertEquals(ArquivoProcessa.getVendedor(),2);
 		assertEquals(ArquivoProcessa.getIdVenda(),1);
 	}
 	@Test
 	public void testeProcessamentoMultiplo() {
-		
+		ArquivosManager.flag = true;
+		ArquivoProcessa.processaArquivos(DiretorioWatchInter.DIRETORIO_IN+"\\teste.txt");
+		assertEquals(ArquivoProcessa.getCliente(),2);
+		assertEquals(ArquivoProcessa.getVendedor(),2);
+		assertEquals(ArquivoProcessa.getIdVenda(),1);
+		ArquivosManager.flag = true;
+		ArquivoProcessa.processaArquivos(DiretorioWatchInter.DIRETORIO_IN+"\\teste2.txt");
+		assertEquals(ArquivoProcessa.getCliente(),3);
+		assertEquals(ArquivoProcessa.getVendedor(),1);
+		assertEquals(ArquivoProcessa.getIdVenda(),5);
 	}
 
 }
